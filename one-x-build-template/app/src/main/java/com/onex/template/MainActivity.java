ee756c33e86513ecb441ccb98619280f7d36f3aa
Kalilo <elkalebanquier@gmail.com>
2026-09-13 14:59:35 +0000

    ONE-X: add one-x-build-template/app/src/main/java/com/onex/template/MainActivity.java

---
+++ one-x-build-template/app/src/main/java/com/onex/template/MainActivity.java
@@ -0,0 +1,22 @@
+package com.onex.template;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.getSettings().setAllowContentAccess(false);
+        webView.getSettings().setAllowFileAccessFromFileURLs(false);
+        webView.getSettings().setAllowUniversalAccessFromFileURLs(false);
+        webView.loadUrl("file:///android_asset/index.html");
+        setContentView(webView);
+    }
+}
