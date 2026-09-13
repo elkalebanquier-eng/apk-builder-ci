a092edf5549b9de3ba2a1ca924f5503c6d5b3ba4
Kalilo <elkalebanquier@gmail.com>
2026-09-13 15:17:22 +0000

    ONE-X: support app display settings

--- one-x-build-template/app/src/main/java/com/onex/template/MainActivity.java
+++ one-x-build-template/app/src/main/java/com/onex/template/MainActivity.java
@@ -4,11 +4,26 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if ("false".equals("ONE_X_STATUS_BAR")) {
+            getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            );
+        }
+        if ("true".equals("ONE_X_FULLSCREEN")) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            );
+        }
         WebView webView = new WebView(this);
         webView.setWebViewClient(new WebViewClient());
         webView.getSettings().setJavaScriptEnabled(true);
